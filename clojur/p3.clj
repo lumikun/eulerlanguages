06a3b140b1b294642e977e4d9c379a4d325993ce
Māris Muižnieks <maris.muiznieks@lumi.moe>
2021-02-26 21:57:09 +0200

    Update p3.clj

--- clojur/p3.clj
+++ clojur/p3.clj
@@ -1,11 +1,14 @@
+
 (defn prime? [n]
-    (.isProbablePrime (bigint n) 10)))
+    "Takes [n] and tests if it's a prime?"
+    (.isProbablePrime (.toBigInteger (bigint n)) 10)) ; Had to resort to a dirty hack to get this to work. 
 
 (defn solve[]
+    "Solves the 3rd Problem from Project Euler."
     (let [n 600851475143]
         (loop [i (int (sort n))]
             (if (and (zero? (rem n i)) (prime? i))
                 i
                 (recur (dec i))))))
-
+                
 (println (solve))
