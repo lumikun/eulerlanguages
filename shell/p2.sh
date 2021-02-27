6abc2b8d02e99fb30dbee35ca7dd4f68a193c3c0
Māris Muižnieks <maris.muiznieks@lumi.moe>
2021-02-27 14:44:44 +0200

    Create p2.sh

---
+++ shell/p2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+function fibs
+{
+    ans=0
+    x=0
+    y=1
+    i=2
+    while [$i -lt $n]
+    do
+        i=`expr $i + 1`
+        z=`expr $x + $y`
+        x=$y
+        y=$z
+        if [$y % 2] 
+        then
+            ans=`expr $y + $ans`
+        fi
+    done
+    ans=$y
+}
+n=4000000
+ans=`fibs $n`
