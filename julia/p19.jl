6fc562cca2c744adc5196507b2f137fed03312b4
Māris Muižnieks <maris.muiznieks@lumi.moe>
2021-03-14 19:43:38 +0200

    2 more solutions

---
+++ julia/p19.jl
@@ -0,0 +1,9 @@
+"""
+    Problem 19
+    How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+
+    Since I'm a cheeky bastard then i just did the Mathematical thing 1200/7 = 171
+"""
+
+ans = Int(round(1200/7))
+println(ans)
