e07a0d5c627be908defea5235428651c446eb139
Māris Muižnieks <maris.muiznieks@lumi.moe>
2021-03-20 18:34:19 +0200

    2 more solutions in Julia

---
+++ julia/p20.jl
@@ -0,0 +1,11 @@
+"""
+    Problem 18
+    Find the sum of the digits in the number 100!
+"""
+
+function solve()
+    ans = factorial(big(100))
+    ans = parse.(Int, collect(string(ans)))
+    return sum(ans)
+end
+println(solve())
---
+++ julia/p21.jl
@@ -0,0 +1,13 @@
+"""
+    Problem 18
+    Evaluate the sum of all the amicable numbers under 10000.
+"""
+
+using Primes, Combinatorics
+
+d(n) = sum(Set(map(prod,combinations(vcat([1],map(x -> fill(x[1],x[2]),collect(factor(n)))...)))))-n
+function solve(n)
+    sum(filter(x-> x == d(d(x)) && d(x) != x,1:n-1))
+end
+
+println(solve(10000))
